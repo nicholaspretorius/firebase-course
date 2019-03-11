b903a3afe989991f72de1fd74510be1018c65d54
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-11 12:04:08 +0200

    Updating to AngularFire.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,24 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-
-import { Course } from './../model/course';
-
-const config = {
-  apiKey: "AIzaSyD4279I3tW_lTgeK4U4oeJZSu176YJTFEA",
-  authDomain: "fb-course-ncp.firebaseapp.com",
-  databaseURL: "https://fb-course-ncp.firebaseio.com",
-  projectId: "fb-course-ncp",
-  storageBucket: "fb-course-ncp.appspot.com",
-  messagingSenderId: "746287073234"
-};
-
-firebase.initializeApp(config);
-
-const db = firebase.firestore();
-const settings = {timestampsInSnapshots: true};
-
-db.settings(settings);
+import {AngularFirestore} from "@angular/fire/firestore";
 
 @Component({
   selector: 'about',
@@ -27,26 +8,14 @@ db.settings(settings);
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-
-    db.collection('courses')
-      .get()
-      .then(snaps => {
-          let courses: Course[] = snaps.docs.map(snap => {
-            return <Course>{
-              id: snap.id,
-              ...snap.data(),
-            };
-          });
-          console.log('Courses: ', courses);
-        }
-      );
-
-    // db.doc('courses/0M6CZTfj8RQ8lZH8Sb2Q')
-    //   .get()
-    //   .then(db => console.log(db.data()));
+    this.db.collection('courses')
+      .valueChanges()
+      .subscribe(val => {
+        console.log(val)
+      });
   }
 
 }
