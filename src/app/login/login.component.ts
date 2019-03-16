d95a97584d3948ff3dd16c8d2d681b45facb3312
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-16 12:17:35 +0200

    Adding Firebase auth.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import * as firebaseui from 'firebaseui';
 import * as firebase from 'firebase/app';
 import {AngularFireAuth} from "@angular/fire/auth";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -9,11 +10,11 @@ import {AngularFireAuth} from "@angular/fire/auth";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   ui: firebaseui.auth.AuthUI;
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router, private ngZone: NgZone) { }
 
   ngOnInit() {
     const uiConfig = {
@@ -30,6 +31,13 @@ export class LoginComponent implements OnInit {
     this.ui.start('#firebaseui-auth-container', uiConfig);
   }
 
-  onLoginSuccess() {}
+  ngOnDestroy(): void {
+    this.ui.delete();
+  }
+
+  onLoginSuccess(result) {
+    console.log('Firebase UI Login Result: ', result);
+    this.ngZone.run(() => this.router.navigateByUrl('/courses'));
+  }
 
 }
