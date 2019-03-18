59e8567caf71e3e1d846764cfcf9dd08f3b669d8
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-18 09:29:59 +0200

    Added a profile icon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit{
 
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
+  pictureUrl$: Observable<string>;
 
     constructor(private afAuth: AngularFireAuth) {}
 
@@ -22,6 +23,7 @@ export class AppComponent implements OnInit{
 
       this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
       this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+      this.pictureUrl$ = this.afAuth.authState.pipe(map(user => user ? user.photoURL : null));
     }
 
     logout() {
