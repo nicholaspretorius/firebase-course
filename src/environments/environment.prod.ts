96eb395e7f9075b44dfb07f55b1bd5e7483de63f
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-20 10:14:25 +0200

    Added Firebase hosting.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // typically you would use a different db in production
+  firebase: {
+    apiKey: "AIzaSyD4279I3tW_lTgeK4U4oeJZSu176YJTFEA",
+    authDomain: "fb-course-ncp.firebaseapp.com",
+    databaseURL: "https://fb-course-ncp.firebaseio.com",
+    projectId: "fb-course-ncp",
+    storageBucket: "fb-course-ncp.appspot.com",
+    messagingSenderId: "746287073234"
+  }
 };
