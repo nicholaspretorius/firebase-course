0d4a7d96795a5d5247b5e295d9b860030f8a90fd
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-22 08:31:57 +0200

    Adding Firebase Cloud functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,14 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// export const helloWorld = functions.https.onRequest((req, res) => {
+//  res.status(200).json({message: "Hello from Firebase!"});
+// });
+
+const express = require('express');
+
+const app = express();
+
+app.get('/courses', (req, res) => {
+
 });
