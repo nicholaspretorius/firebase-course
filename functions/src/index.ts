7dc457d371f936b5ff29f13578cf31e8b6debf51
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-22 08:39:47 +0200

    Adding Express server.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import {db} from "./init";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,10 +8,20 @@ import * as functions from 'firebase-functions';
 //  res.status(200).json({message: "Hello from Firebase!"});
 // });
 
-const express = require('express');
+import * as express from "express";
+const cors = require('cors');
 
 const app = express();
+app.use(cors({origin: true}));
 
-app.get('/courses', (req, res) => {
+app.get('/courses', async (req, res) => {
+  const snaps = await db.collection('courses').get();
+  const courses = [];
+
+  snaps.forEach(snap => courses.push(snap.data()));
+
+  res.status(200).json({courses});
 
 });
+
+export const getCourses = functions.https.onRequest(app);
