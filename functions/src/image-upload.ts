b340d32da097a6d34b84268b2791f7d3ef74f93b
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-23 15:08:19 +0200

    Adding image download.

--- functions/src/image-upload.ts
+++ functions/src/image-upload.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
 const path = require('path');
+const os = require('os');
+const { Storage } = require('@google-cloud/storage');
+const mkdirp = require('mkdirp-promise');
+
+const gcs = new Storage();
 
 export const resizeThumbnail = functions.storage.object()
   .onFinalize(async (object, context) => {
@@ -7,13 +12,27 @@ export const resizeThumbnail = functions.storage.object()
     const fileFullPath = object.name || '',
       contentType = object.contentType || '',
       fileDirectory = path.dirname(fileFullPath),
-      fileName = path.basename(fileFullPath);
+      fileName = path.basename(fileFullPath),
+      tempLocalDir = path.join(os.tmpdir(), fileDirectory);
 
       console.log('Thumbnail generation started: ', fileFullPath, contentType, fileDirectory, fileName);
 
       if (!contentType.startsWith('image/')) {
+        console.log('Exiting image processing...');
         return null;
       }
+
+      await mkdirp(tempLocalDir);
+
+      const bucket = gcs.bucket(object.bucket);
+
+      const originalImageFile = bucket.file(fileFullPath);
+
+      const tempLocalFile = path.join(os.tmpdir(), fileFullPath);
+
+      console.log('Downloading image to: ', tempLocalFile);
+
+      await originalImageFile.download({destination: tempLocalFile});
 
       return null;
   });
